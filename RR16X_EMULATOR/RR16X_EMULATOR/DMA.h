236f7c702bf834bfdec033854b5314c1300d8953
Alex <82306660+alextron700@users.noreply.github.com>
2026-07-24 09:56:42 +1000

    Modify DMA class to include handler address
    Added handler_address to DMA class constructor and private members.

--- RR16X_EMULATOR/RR16X_EMULATOR/DMA.h
+++ RR16X_EMULATOR/RR16X_EMULATOR/DMA.h
@@ -7,13 +7,14 @@ class DMA :
     public AbstractPeripheral
 {
 public:
-    DMA(bus& system_bus, InterruptEnhancer& ie);
+    DMA(bus& system_bus, InterruptEnhancer& ie, uint32_t handler);
     uint16_t read(uint32_t address) override;
     void write(uint32_t address, uint16_t value) override;
     void tick() override;
 private:
     bus& Bus;
     InterruptEnhancer& enhancer;
+    uint32_t handler_address;
     uint32_t src_address = 0;
     uint32_t dest_address = 0;
     uint16_t count = 0;
