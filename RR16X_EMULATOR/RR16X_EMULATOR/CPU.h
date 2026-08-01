e89c7b1ed5de23f347c9a6e7c34ccfdb3c2c8118
Alex <82306660+alextron700@users.noreply.github.com>
2026-08-01 21:28:35 +1000

    e

--- RR16X_EMULATOR/RR16X_EMULATOR/CPU.h
+++ RR16X_EMULATOR/RR16X_EMULATOR/CPU.h
@@ -13,11 +13,10 @@ public:
 	// dump current state to configurable output
 	void dumpState(std::ostream& out = std::cout);
 	// returns all registers
-	std::vector<uint16_t> getRegs() const
-	{
-	const std::vector<uint16_t> l = this->registers;
-	return l;
-	}
+	std::vector<uint16_t> CPU::getRegs() const
+{
+    return this->registers;
+}
 	// returns the program counter
 	uint32_t getPC(const bool includeProgramEAM = true) const
 	{
